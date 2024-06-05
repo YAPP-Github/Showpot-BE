4df94b2b52ecd09d96c83624a23848e40998547e
GaBaljaintheroom <junsu1222@naver.com>
2024-06-05 23:47:57 +0900

    feat : jwt 관련 의존성 및 코드 user-api 모듈로 이동

--- app/api/src/main/java/org/example/config/ApiConfig.java
+++ app/api/src/main/java/org/example/config/ApiConfig.java
@@ -1,14 +1,11 @@
 package org.example.config;
 
-import org.example.property.TokenProperty;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({UserApiConfig.class, InfraStructureConfig.class})
-@EnableConfigurationProperties(TokenProperty.class)
+@Import(UserApiConfig.class)
 @ComponentScan(basePackages = "org.example")
 public class ApiConfig {
 
--- app/api/user-api/src/main/java/org/example/config/UserApiConfig.java
+++ app/api/user-api/src/main/java/org/example/config/UserApiConfig.java
@@ -1,11 +1,14 @@
 package org.example.config;
 
+import org.example.property.TokenProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import(UserDomainConfig.class)
+@Import({UserDomainConfig.class, InfraStructureConfig.class})
+@EnableConfigurationProperties(TokenProperty.class)
 @ComponentScan(basePackages = "org.example")
 public class UserApiConfig {
 
