5b8b500b2d2d6d7760495e3a8af5c3bd996a3490
GaBaljaintheroom <junsu1222@naver.com>
2024-06-16 01:55:17 +0900

    feat : SubscribeGenre 도메인 엔티티 추가

---
+++ app/domain/subscribe-genre-domain/src/main/java/org/example/entity/SubscribeGenre.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "app_subscribe_genre")
+public class SubscribeGenre extends BaseEntity {
+
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(name = "genre_id", nullable = false)
+    private UUID genreId;
+}
