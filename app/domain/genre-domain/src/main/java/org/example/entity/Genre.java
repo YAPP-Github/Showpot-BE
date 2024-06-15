a4e235a7af95e827345d40180f9c9a79d6d8395e
GaBaljaintheroom <junsu1222@naver.com>
2024-06-16 01:50:12 +0900

    feat : Genre 도메인 엔티티 추가

---
+++ app/domain/genre-domain/src/main/java/org/example/entity/Genre.java
@@ -0,0 +1,26 @@
+package org.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "app_genre")
+public class Genre extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "show_id")
+    private Show show;
+
+}
