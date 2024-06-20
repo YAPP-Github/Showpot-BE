f059b15cd78c50284e77571debb1f302d091901e
GaBaljaintheroom <junsu1222@naver.com>
2024-06-20 12:31:04 +0900

    refactor : Artist, Genre 의미 없는 관계 삭제

--- app/domain/show-domain/src/main/java/org/example/entity/artist/Artist.java
+++ app/domain/show-domain/src/main/java/org/example/entity/artist/Artist.java
@@ -2,15 +2,11 @@ package org.example.entity.artist;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.entity.BaseEntity;
-import org.example.entity.show.Show;
 
 @Entity
 @Getter
@@ -26,9 +22,5 @@ public class Artist extends BaseEntity {
 
     @Column(name = "country", nullable = false)
     private String country;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "show_id")
-    private Show show;
 
 }
--- app/domain/show-domain/src/main/java/org/example/entity/genre/Genre.java
+++ app/domain/show-domain/src/main/java/org/example/entity/genre/Genre.java
@@ -2,15 +2,11 @@ package org.example.entity.genre;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.entity.BaseEntity;
-import org.example.entity.show.Show;
 
 @Entity
 @Getter
@@ -20,9 +16,5 @@ public class Genre extends BaseEntity {
 
     @Column(name = "name", nullable = false)
     private String name;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "show_id")
-    private Show show;
 
 }
