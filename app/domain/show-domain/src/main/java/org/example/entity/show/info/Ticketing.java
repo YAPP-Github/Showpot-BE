8491094878da0ee8e16dd5df5d7b507218e6e3d1
GaBaljaintheroom <junsu1222@naver.com>
2024-06-21 01:10:17 +0900

    refactor : Ticketing Map 키 타입 수정

--- app/domain/show-domain/src/main/java/org/example/entity/show/info/Ticketing.java
+++ app/domain/show-domain/src/main/java/org/example/entity/show/info/Ticketing.java
@@ -5,7 +5,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.util.HashMap;
 import java.util.Map;
-import org.example.vo.TicketBookingSite;
 import org.hibernate.annotations.Type;
 
 @Embeddable
@@ -13,10 +12,10 @@ public class Ticketing {
 
     @Type(JsonType.class)
     @Column(name = "ticketing", columnDefinition = "jsonb", nullable = false)
-    private Map<TicketBookingSite, String> ticketingInformation = new HashMap<>();
+    private Map<String, String> ticketingInformation = new HashMap<>();
 
     public void saveTicketingInformation(
-        TicketBookingSite ticketBookingSite,
+        String ticketBookingSite,
         String ticketingSiteUrl
     ) {
         ticketingInformation.put(ticketBookingSite, ticketingSiteUrl);
