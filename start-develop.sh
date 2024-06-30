1f66c5b5d6a8fa567fba4f824142d46b08928cfc
devmizz <garamminchan@gmail.com>
2024-06-30 17:15:46 +0900

    feat: docker-compose & start.sh

---
+++ start-develop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Git 리포지토리에서 최신 코드를 가져옵니다.
+git pull origin develop
+
+# 애플리케이션 빌드
+./gradlew build
+
+# Docker Compose를 사용하여 컨테이너 실행
+docker-compose down
+docker-compose up -d --build
