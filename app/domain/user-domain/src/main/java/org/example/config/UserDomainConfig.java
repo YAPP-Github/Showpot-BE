72ccf986e8383ba423fbe3061e2a55efc05d9732
GaBaljaintheroom <junsu1222@naver.com>
2024-07-10 23:43:17 +0900

    refactor : User 도메인 수정

--- app/domain/user-domain/src/main/java/org/example/config/UserDomainConfig.java
+++ app/domain/user-domain/src/main/java/org/example/config/UserDomainConfig.java
@@ -8,7 +8,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @ComponentScan(basePackages = "org.example")
 @EntityScan(basePackages = "org.example.entity")
-@EnableJpaRepositories(basePackages = "org.example.repository.user")
+@EnableJpaRepositories(basePackages =
+    {"org.example.repository.user", "org.example.repository.admin"})
 public class UserDomainConfig {
 
 }
