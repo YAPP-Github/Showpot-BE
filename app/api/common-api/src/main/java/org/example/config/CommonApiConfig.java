28ff5614e464ab78c5011935d01ef1ecfa2490da
GaBaljaintheroom <junsu1222@naver.com>
2024-07-10 22:57:38 +0900

    refactor : admin 로그인 뷰 기능 삭제

--- app/api/common-api/src/main/java/org/example/config/CommonApiConfig.java
+++ app/api/common-api/src/main/java/org/example/config/CommonApiConfig.java
@@ -1,13 +1,12 @@
 package org.example.config;
 
 import org.example.property.TokenProperty;
-import org.example.property.UserAdminProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({TokenProperty.class, UserAdminProperty.class})
+@EnableConfigurationProperties(TokenProperty.class)
 @ComponentScan(basePackages = "org.example")
 public class CommonApiConfig {
 
