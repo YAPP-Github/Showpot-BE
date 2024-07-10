d866675b1f9d0fc2991eca4c95271783d4677776
GaBaljaintheroom <junsu1222@naver.com>
2024-07-10 23:32:27 +0900

    feat : admin 엔티티 생성

---
+++ app/domain/user-domain/src/main/java/org/example/entity/Admin.java
@@ -0,0 +1,29 @@
+package org.example.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "app_admin")
+public class Admin extends BaseEntity {
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Builder
+    private Admin(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+}
