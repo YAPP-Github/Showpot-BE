71734ef3b220d0c02cefd29d08667b0f61521cfb
GaBaljaintheroom <junsu1222@naver.com>
2024-07-19 00:24:32 +0900

    test : 어드민 공연 서비스 테스트 작성

---
+++ app/api/show-api/src/test/java/show/fixture/dto/ShowDtoFixture.java
@@ -0,0 +1,71 @@
+package show.fixture.dto;
+
+import com.example.show.controller.dto.response.SeatInfoApiResponse;
+import com.example.show.controller.dto.response.TicketingInfoApiResponse;
+import com.example.show.service.dto.request.ShowCreateServiceRequest;
+import com.example.show.service.dto.request.ShowUpdateServiceRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import org.springframework.mock.web.MockMultipartFile;
+
+public class ShowDtoFixture {
+
+    private static final MockMultipartFile post = new MockMultipartFile(
+        "image",
+        "test_image.jpg",
+        "image/jpeg",
+        "test image content".getBytes()
+    );
+
+    public static ShowCreateServiceRequest showCreateServiceRequest() {
+        return ShowCreateServiceRequest.builder()
+            .title("test_title")
+            .content("test_content")
+            .date(LocalDate.EPOCH)
+            .location("test_location")
+            .post(post)
+            .seatInfoApiResponse(getSeatInfoApiResponse())
+            .ticketingInfoApiResponse(getTicketingInfoApiResponse())
+            .artistIds(List.of(UUID.randomUUID()))
+            .genreIds(List.of(UUID.randomUUID()))
+            .build();
+    }
+
+    public static ShowUpdateServiceRequest showUpdateServiceRequest() {
+        return ShowUpdateServiceRequest.builder()
+            .title("test_title")
+            .content("test_content")
+            .date(LocalDate.EPOCH)
+            .location("test_location")
+            .post(post)
+            .seatInfoApiResponse(getSeatInfoApiResponse())
+            .ticketingInfoApiResponse(getTicketingInfoApiResponse())
+            .artistIds(List.of(UUID.randomUUID()))
+            .genreIds(List.of(UUID.randomUUID()))
+            .build();
+    }
+
+    private static SeatInfoApiResponse getSeatInfoApiResponse() {
+        return new SeatInfoApiResponse(
+            Map.of(
+                "VIP", 150000,
+                "R", 120000,
+                "S", 100000,
+                "A", 80000
+            )
+        );
+    }
+
+    private static TicketingInfoApiResponse getTicketingInfoApiResponse() {
+        return new TicketingInfoApiResponse(
+            LocalDateTime.MAX,
+            Map.of(
+                "YES24", "https://YES24URL",
+                "인터파크", "https://인터파크URL"
+            )
+        );
+    }
+}
