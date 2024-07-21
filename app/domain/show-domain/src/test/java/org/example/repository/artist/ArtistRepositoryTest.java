dda900416eb30d66f1add0ce09c270cb812cb36b
devmizz <garamminchan@gmail.com>
2024-07-22 01:28:44 +0900

    fix: Update test yml

--- app/domain/show-domain/src/test/java/org/example/repository/artist/ArtistRepositoryTest.java
+++ app/domain/show-domain/src/test/java/org/example/repository/artist/ArtistRepositoryTest.java
@@ -13,10 +13,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-@ActiveProfiles("show-domain-test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class ArtistRepositoryTest {
 
