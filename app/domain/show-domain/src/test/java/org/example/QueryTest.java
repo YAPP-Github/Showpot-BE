6dbea46f8084702199631570716811e2909d9fad
GaBaljaintheroom <junsu1222@naver.com>
2024-07-23 12:22:01 +0900

    feat : 테스트 컨테이너 추가

--- app/domain/show-domain/src/test/java/org/example/QueryTest.java
+++ app/domain/show-domain/src/test/java/org/example/QueryTest.java
@@ -2,9 +2,12 @@ package org.example;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
 
 @DataJpaTest
+@ActiveProfiles("show-test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class QueryTest {
+public class QueryTest extends DatabaseContainer {
 
 }
