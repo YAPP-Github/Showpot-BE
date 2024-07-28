d02dbcc63808f7428e2a4a14e20b71276c2576f8
GaBaljaintheroom <junsu1222@naver.com>
2024-07-29 00:37:43 +0900

    refactor : 중복 클래스 제거

--- app/domain/show-domain/src/main/java/org/example/repository/show/ShowSearchQuerydslRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository.show;
-
-import org.example.dto.show.request.ShowSearchPaginationDomainRequest;
-import org.example.dto.show.response.ShowSearchPaginationDomainResponse;
-
-public interface ShowSearchQuerydslRepository {
-
-    ShowSearchPaginationDomainResponse searchShow(ShowSearchPaginationDomainRequest request);
-}
