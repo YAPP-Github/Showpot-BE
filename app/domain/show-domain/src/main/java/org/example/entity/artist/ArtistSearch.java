6d26c202e76954eeb0caefae8dbbe3446a325d8a
GaBaljaintheroom <junsu1222@naver.com>
2024-08-05 18:46:44 +0900

    refactor : 아티스트 업데이트 어드민 수정

--- app/domain/show-domain/src/main/java/org/example/entity/artist/ArtistSearch.java
+++ app/domain/show-domain/src/main/java/org/example/entity/artist/ArtistSearch.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -29,5 +30,23 @@ public class ArtistSearch extends BaseEntity {
     private ArtistSearch(String name, Artist artist) {
         this.name = name;
         this.artist = artist;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ArtistSearch that = (ArtistSearch) o;
+        return Objects.equals(getName(), that.getName()) && Objects.equals(
+            getArtist(), that.getArtist());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getArtist());
     }
 }
