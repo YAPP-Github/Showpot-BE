b15461767fb677b467787ff0d56c8663182d59b5
GaBaljaintheroom <junsu1222@naver.com>
2024-08-07 00:03:14 +0900

    refactor : DatetimeFormatter 요일 삭제

--- common/src/main/java/org/example/util/DateTimeUtil.java
+++ common/src/main/java/org/example/util/DateTimeUtil.java
@@ -2,20 +2,14 @@ package org.example.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
-import java.util.Locale;
 
 public class DateTimeUtil {
 
     public static String formatLocalDateTime(LocalDateTime dateTime) {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-M-d");
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-        String dayOfWeek = dateTime
-            .getDayOfWeek()
-            .getDisplayName(TextStyle.FULL, Locale.KOREAN)
-            .substring(0, 1);
 
-        return dateTime.format(dateFormatter) + " (" + dayOfWeek + ") " + dateTime.format(timeFormatter);
+        return dateTime.format(dateFormatter) + dateTime.format(timeFormatter);
     }
 
 }
