e233adb3669127b92c5a53508de13c0d1d1af34d
GaBaljaintheroom <junsu1222@naver.com>
2024-08-07 22:05:51 +0900

    refactor : 로그아웃/회원탈퇴 필수 요청 값 식별 추가

--- app/api/user-api/src/main/java/org/example/controller/dto/request/LogoutApiRequest.java
+++ app/api/user-api/src/main/java/org/example/controller/dto/request/LogoutApiRequest.java
@@ -1,9 +1,11 @@
 package org.example.controller.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import org.example.service.dto.request.LogoutServiceRequest;
 
 public record LogoutApiRequest(
+    @NotNull(message = "accessToken은 필수 입력값입니다.")
     String accessToken
 ) {
 
--- app/api/user-api/src/main/java/org/example/controller/dto/request/WithdrawalApiRequest.java
+++ app/api/user-api/src/main/java/org/example/controller/dto/request/WithdrawalApiRequest.java
@@ -1,9 +1,11 @@
 package org.example.controller.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import org.example.service.dto.request.WithdrawalServiceRequest;
 
 public record WithdrawalApiRequest(
+    @NotNull(message = "accessToken은 필수 입력값입니다.")
     String accessToken
 ) {
 
