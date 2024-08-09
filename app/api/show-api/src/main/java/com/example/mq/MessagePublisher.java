955e10c6e6b92e41b9a4ecba18fb75cb906a1c78
GaBaljaintheroom <junsu1222@naver.com>
2024-08-10 01:24:52 +0900

    feat : 장르 구독 후 메시지 발행

--- app/api/show-api/src/main/java/com/example/mq/MessagePublisher.java
+++ app/api/show-api/src/main/java/com/example/mq/MessagePublisher.java
@@ -1,6 +1,7 @@
 package com.example.mq;
 
 import com.example.mq.message.ArtistSubscriptionServiceMessage;
+import com.example.mq.message.GenreSubscriptionServiceMessage;
 import com.example.mq.message.ShowRelationArtistAndGenreServiceMessage;
 import java.util.List;
 
@@ -9,4 +10,6 @@ public interface MessagePublisher {
     void publishShow(String topic, ShowRelationArtistAndGenreServiceMessage message);
 
     void publishArtistSubscription(String topic, List<ArtistSubscriptionServiceMessage> messages);
+
+    void publishGenreSubscription(String topic, List<GenreSubscriptionServiceMessage> messages);
 }
--- app/infrastructure/mq/src/main/java/org/example/publish/RedisMessagePublisher.java
+++ app/infrastructure/mq/src/main/java/org/example/publish/RedisMessagePublisher.java
@@ -2,11 +2,13 @@ package org.example.publish;
 
 import com.example.mq.MessagePublisher;
 import com.example.mq.message.ArtistSubscriptionServiceMessage;
+import com.example.mq.message.GenreSubscriptionServiceMessage;
 import com.example.mq.message.ShowRelationArtistAndGenreServiceMessage;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.message.ArtistSubscriptionInfraMessage;
+import org.example.message.GenreSubscriptionInfraMessage;
 import org.example.message.ShowRelationArtistAndGenreInfraMessage;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -40,5 +42,19 @@ public class RedisMessagePublisher implements MessagePublisher {
         template.convertAndSend(topic, infraMessages);
         log.info("Message published successfully to topic: {}", topic);
         log.info("Message Contents ( artistSubscriptionMessage : {} )", infraMessages);
+    }
+
+    @Override
+    public void publishGenreSubscription(
+        String topic,
+        List<GenreSubscriptionServiceMessage> messages
+    ) {
+        var infraMessages = messages.stream()
+            .map(GenreSubscriptionInfraMessage::from)
+            .toList();
+
+        template.convertAndSend(topic, infraMessages);
+        log.info("Message published successfully to topic: {}", topic);
+        log.info("Message Contents ( genreSubscriptionMessage : {} )", infraMessages);
     }
 }
