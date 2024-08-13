cb4ec737e1ddb854c208beee11b27071998c2d9f
GaBaljaintheroom <junsu1222@naver.com>
2024-08-14 01:34:35 +0900

    feat: 문서화 추가

--- app/api/show-api/src/main/java/com/example/show/controller/dto/response/TicketingAlertReservationApiResponse.java
+++ app/api/show-api/src/main/java/com/example/show/controller/dto/response/TicketingAlertReservationApiResponse.java
@@ -1,11 +1,15 @@
 package com.example.show.controller.dto.response;
 
 import com.example.show.service.dto.response.TicketingAlertReservationServiceResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record TicketingAlertReservationApiResponse(
+    @Schema(description = "공연 알림 예약 상태")
     TicketingAlertReservationStatusApiResponse alertReservationStatus,
+
+    @Schema(description = "공연 알림 예약 가능 여부")
     TicketingAlertReservationAvailabilityApiResponse alertReservationAvailability
 ) {
     public static TicketingAlertReservationApiResponse from(
--- app/api/show-api/src/main/java/com/example/show/controller/dto/response/TicketingAlertReservationStatusApiResponse.java
+++ app/api/show-api/src/main/java/com/example/show/controller/dto/response/TicketingAlertReservationStatusApiResponse.java
@@ -1,12 +1,19 @@
 package com.example.show.controller.dto.response;
 
 import com.example.show.service.dto.response.TicketingAlertReservationStatusServiceResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record TicketingAlertReservationStatusApiResponse(
+
+    @Schema(description = "공연 티켓팅 24 시간 전")
     boolean before24,
+
+    @Schema(description = "공연 티켓팅 6 시간 전")
     boolean before6,
+
+    @Schema(description = "공연 티켓팅 1 시간 전")
     boolean before1
 ) {
 
