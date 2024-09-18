434accb72b51aef7aee0c39a08f68174d41f9820
GaBaljaintheroom <junsu1222@naver.com>
2024-09-19 01:09:17 +0900

    fix: swagger 스키마 설명 누락 추가

--- app/api/show-api/src/main/java/com/example/genre/controller/dto/param/GenreSubscriptionPaginationApiParam.java
+++ app/api/show-api/src/main/java/com/example/genre/controller/dto/param/GenreSubscriptionPaginationApiParam.java
@@ -1,10 +1,14 @@
 package com.example.genre.controller.dto.param;
 
 import com.example.genre.service.dto.param.GenreSubscriptionPaginationServiceParam;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 
 public record GenreSubscriptionPaginationApiParam(
+    @Schema(description = "장르 ID")
     UUID id,
+
+    @Schema(description = "장르 이름")
     String name
 ) {
 
