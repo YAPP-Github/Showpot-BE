1f887ea637b167b33fbdeda0bc8a41c4447a8a6d
devmizz <garamminchan@gmail.com>
2024-09-28 00:04:37 +0900

    feat: Add swagger description

--- app/api/user-api/src/main/java/org/example/controller/dto/response/HasUnreadNotificationApiResponse.java
+++ app/api/user-api/src/main/java/org/example/controller/dto/response/HasUnreadNotificationApiResponse.java
@@ -1,9 +1,11 @@
 package org.example.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record HasUnreadNotificationApiResponse(
+    @Schema(description = "안 읽은 알림 존재 여부")
     boolean hasUnreadNotification
 ) {
 
--- app/api/user-api/src/main/java/org/example/controller/dto/response/SimpleNotificationApiResponse.java
+++ app/api/user-api/src/main/java/org/example/controller/dto/response/SimpleNotificationApiResponse.java
@@ -1,14 +1,20 @@
 package org.example.controller.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 import lombok.Builder;
 
 @Builder
 public record SimpleNotificationApiResponse(
+    @Schema(description = "알림 제목")
     String title,
+    @Schema(description = "알림 본문")
     String message,
+    @Schema(description = "알림 시간")
     String notifiedAt,
+    @Schema(description = "공연 ID")
     UUID showId,
+    @Schema(description = "공연 이미지 URL")
     String showImageURL
 ) {
 
